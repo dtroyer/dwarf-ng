1ef8295300d12471e58dcfb05bccb5ba53adc4f2
Dean Troyer <dtroyer@gmail.com>
2015-04-18 10:58:18 -0500

    Add Identity version discovery responses
    Add routes for Identity '/' to respond with supported versions

--- dwarf/identity/api.py
+++ dwarf/identity/api.py
@@ -99,6 +99,43 @@ TOKENS_RESPONSE = {
 }
 
 
+VERSIONS_RESPONSE = {
+    "versions": {
+        "values": [
+            {
+                "status": "stable",
+                "updated": "2014-04-17T00:00:00Z",
+                "media-types": [
+                    {
+                        "base": "application/json",
+                        "type": "application/vnd.openstack.identity-v2.0+json",
+                    },
+                ],
+                "id": "v2.0",
+                "links": [
+                    {
+                        "href": "http://%s:%s/v2.0/" % (CONF.bind_host, CONF.identity_api_port),
+                        "rel": "self",
+                    },
+                ],
+            },
+        ],
+    },
+}
+
+
+@exception.catchall
+def _route_versions():
+    """
+    Route:  /
+    Method: GET
+    """
+    utils.show_request(bottle.request)
+
+    bottle.response.status = 300
+    return VERSIONS_RESPONSE
+
+
 @exception.catchall
 def _route_tokens():
     """
@@ -118,6 +155,9 @@ class _IdentityApiServer(api_server.ApiServer):
                                                  CONF.bind_host,
                                                  CONF.identity_api_port)
 
+        self.app.route("/",
+                       method="GET",
+                       callback=_route_versions)
         self.app.route('/v2.0/tokens',
                        method='POST',
                        callback=_route_tokens)
