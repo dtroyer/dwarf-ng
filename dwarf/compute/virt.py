91801d2b745321bc62ffd720f7191cf7d597c972
Dean Troyer <dtroyer@gmail.com>
2017-12-18 16:24:28 -0600

    Create libvirt domains using server name
    This has a name-uniqueness problem as it stands, but for now it is
    much more helpful to see the server name in virsh.

--- dwarf/compute/virt.py
+++ dwarf/compute/virt.py
@@ -48,7 +48,7 @@ _LIBVIRT_DOMAIN_STATE = {
 
 
 def _name(sid):
-    return 'dwarf-%08x' % int(sid)
+    return 'dwarf-%s' % sid
 
 
 def _xml_snippet(name, enable):
@@ -87,7 +87,7 @@ def _create_domain_xml(server, flavor, force=False):
         xml_info = {
             'domain_type': CONF.libvirt_domain_type,
             'uuid': server['id'],
-            'name': _name(server['int_id']),
+            'name': _name(server['name']),
             'memory': int(flavor['ram']) * 1024,
             'vcpus': flavor['vcpus'],
             'basepath': basepath,
